c3449e3dc8e160fdee7865b4c24206eea31e4be5
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-17 23:11:20 +0545

    add data transfer

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ const app = document.getElementById("app");
 app.innerHTML = `
   <h1>Drag And Drop API</h1>
   <div class="uploader"> 
-    <div class="dragme" draggable="true"></div>
-    <div class="dropzone"> 🎯Drag Here!</div>
+    <div id=" item-0"class="dragme" draggable="true"></div>
+    <div  class="dropzone"> 🎯Drag Here!</div>
   </div>
   <style>
     .uploader {
@@ -33,7 +33,7 @@ app.innerHTML = `
       background: #f1f2f5;
     }
     .active {
-      background: #ebfff6;
+      // background: #ebfff6;
       border-color: #24b373;
     }
   </style>
@@ -41,19 +41,33 @@ app.innerHTML = `
 
 //only runs when drag and drop is supported
 const init = () => {
+  const dragMe = document.querySelector(".dragme");
   const dropZone = document.querySelector(".dropzone");
+  dragMe.addEventListener('dragstart', (e) => {
+    e.dataTransfer.setData('text/plain', e.target.id);
+    e.dataTransfer.effectAllowed = 'copy';
+    e.target.classList.add('active');
+  });
   dropZone.addEventListener('dragenter', (e) => e.target.classList.add('active'));
   dropZone.addEventListener('dragleave', (e) => e.target.classList.remove('active'));
   dropZone.addEventListener('dragover', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    e.dataTransfer.dropEffect = 'cop';
+    e.dataTransfer.dropEffect = 'copy';
 
   });
   dropZone.addEventListener('drop', (e) => {
     e.preventDefault();
     e.stopPropagation();
     e.target.classList.remove('active');
+    const id = e.dataTransfer.getData('text/plain');
+    const element = document.getElementById(id);
+    dropZone.appendChild(element);
+    
+    element.addEventListener('click', () => {
+      element.remove();
+    }
+    );
   });
 };
 
